a6470eea64ed3f8f5a897f38375890e9491034a8
Mainto Rudo <anhvu2001ct@gmail.com>
2015-11-22 18:41:19 +0700

    Create MinimapHack.lua

---
+++ Common/MinimapHack.lua
@@ -0,0 +1,62 @@
+--[[ Rx AntiGank | MinimapHack
+     Ver 0.1: Released
+     Go to http://gamingonsteroids.com to Download more script. 
+------------------------------------------------------------------------------------]]
+
+require('Inspired')
+
+---- Script Update ----
+local WebLuaFile = "/anhvu2001ct/Rudo-GoS-Scripts/master/Common/MinimapHack.lua"
+local WebVersion = "/anhvu2001ct/Rudo-GoS-Scripts/master/Common/MinimapHack.version"
+local ScriptName = "MinimapHack.lua"
+local ScriptVersion = 0.1 -- Newest Version
+local CheckWebVer = require("GOSUtility").request("https://raw.githubusercontent.com",WebVersion.."?no-cache="..(math.random(100000))) -- Copy from Inspired >3
+if ScriptVersion < tonumber(CheckWebVer) then
+PrintChat(string.format("<font color='#00B359'>Script need update.</font><font color='#FF2626'> Waiting AutoUpdate.</font>")) 
+AutoUpdate(WebLuaFile,WebVersion,ScriptName,ScriptVersion)
+else
+PrintChat(string.format("<font color='#FFFF26'>You are using newest Version. Don't need to update</font>")) 
+end
+PrintChat(string.format("<font color='#C926FF'>Script Current Version:</font><font color='#FF8000'> %s </font>| <font color='#C926FF'>Newest Version:</font><font color='#FF8000'> %s </font>", ScriptVersion, tonumber(CheckWebVer)))
+
+PrintChat("Credits to Deftsu for MapHack, Inspired for AutoUpdate and Feretofix for Draw Minimap") 
+---------------------------------------------------------------------
+local speed = 0
+local checktime = {}
+local check = {}
+local enemies = {}
+
+MinimapHack = MenuConfig("Rx Minimap Hack", "MinimapHack")
+MinimapHack:Boolean("circle", "Enable Draw Circle", true)
+MinimapHack:ColorPick("col", "Setting Circle Color", {255, 153, 229, 255})
+MinimapHack:Slider("QualiDraw", "Circle Quality", 80, 1, 255, 1)
+MinimapHack:Slider("min", "Circle minium range", 300, 50, 1000, 10)
+MinimapHack:Slider("max", "Circle maximum range", 5400, 4500, 6500, 10)
+
+OnTick(function(myHero)
+enemies = GetEnemyHeroes()
+  for i, enemy in pairs(enemies) do
+   if IsVisible(enemy) == false and IsDead(enemy) == false then
+	 if check[i] == nil then
+	  check[i] = GetTickCount()
+     end
+	 checktime[i] = (GetTickCount() - check[i])/1000
+   else
+   check[i] = nil
+   checktime[i] = 0
+  end
+ end
+end)
+
+OnDrawMinimap(function() 
+    for i, enemy in pairs(enemies) do
+        if IsVisible(enemy) == false and IsDead(enemy) == false and check[i] ~= -1 and checktime ~= 0 then
+          speed = GetMoveSpeed(enemy) * checktime[i]
+           if speed >= MinimapHack.min:Value() and speed <= MinimapHack.max:Value() then
+          if MinimapHack.circle:Value() then DrawCircleMinimap(GetOrigin(enemy), speed, 0, 80, MinimapHack.col:Value()) end
+		   end
+        end
+    end
+end)
+
+PrintChat(string.format("<font color='#FF0000'>Rx AntiGank | GoS MinimapHack </font><font color='#FFFF00'>Version %s: Loaded Success </font><font color='#08F7F3'>Enjoy it and Good Luck :3</font>", ScriptVersion)) 
